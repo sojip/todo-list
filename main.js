5450216fe9e3c9eeea2022788e8f5ee0d56136df
sojip <sojipquantin@gmail.com>
2021-06-21 09:10:51 +0100

    initial setup

---
+++ main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("voila");
+console.log("moi");
+console.log("on est pas ensemble")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTtBQUNBO0FBQ0Esa0MiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwidm9pbGFcIik7XG5jb25zb2xlLmxvZyhcIm1vaVwiKTtcbmNvbnNvbGUubG9nKFwib24gZXN0IHBhcyBlbnNlbWJsZVwiKSJdLCJzb3VyY2VSb290IjoiIn0=
